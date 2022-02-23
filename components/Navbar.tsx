07daeadadf0c33894732e07ce9628caa2c61d49c
Ashhhleyyy <ash@ashhhleyyy.dev>
2022-02-23 21:22:39 +0000

    Change nav bar to use translations

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import styles from '../styles/navbar.module.css';
 import logo from '../public/nucleoid_logo.png';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { T } from './translations';
 
 export default function Navbar() {
     return <nav className={styles.nav}>
@@ -12,8 +13,12 @@ export default function Navbar() {
             </a>
         </Link>
 
-        <NavLink href='/games'>Recent games</NavLink>
-        <NavLink href='/leaderboards'>Leaderboards</NavLink>
+        <NavLink href='/games'>
+            <T k='nucleoid.nav.recent_games' />
+        </NavLink>
+        <NavLink href='/leaderboards'>
+            <T k='nucleoid.nav.leaderboards' />
+        </NavLink>
     </nav>
 }
 
@@ -28,7 +33,9 @@ const NavLink: React.FC<{ href: string }> = ({ href, children }) => {
         </Link>
     } else {
         return <Link href={href}>
-            {children}
+            <a>
+                {children}
+            </a>
         </Link>
     }
 }
